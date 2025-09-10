046a1fe18a01ff8c07487b91d4ef0ec4642ca93a
Gregory Kucherkov <gregorykucherkov@Mac.N4>
2025-09-10 20:38:38 +0300

    argo path for apps

--- modules/argo_cd/argo_cd.tf
+++ modules/argo_cd/argo_cd.tf
@@ -20,7 +20,7 @@ resource "helm_release" "argo_apps" {
   namespace        = var.namespace
   create_namespace = false
   values = [
-    file("${path.module}/values.yaml")
+    file("${path.module}/charts/values.yaml")
   ]
   depends_on = [helm_release.argo_cd]
 }
